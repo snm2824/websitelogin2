1b9edc9a8de4d5e95e60d4a2716e8a429e6889d7
Sonam <66302258+snm2824@users.noreply.github.com>
2021-01-31 15:08:03 +0530

    Add files via upload
    just a basic login page

---
+++ web/index.js
@@ -0,0 +1,12 @@
+function validate(){
+    var userna=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="admin" && password=="admin"){
+        alert("Login successful");
+        return true;
+    }
+        else{
+alert("Lofin failed")
+        }
+    
+}
